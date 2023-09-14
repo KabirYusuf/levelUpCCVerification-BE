c78565390c555489151f245c8a7fc70cfe40688d
KabirYusuf <sonkaybee@gmail.com>
2023-09-14 17:45:06 +0100

    project setup

---
+++ src/main/java/com/levelup/levelUpCCVerificationBE/LevelUpCcVerificationBeApplication.java
@@ -0,0 +1,13 @@
+package com.levelup.levelUpCCVerificationBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelUpCcVerificationBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelUpCcVerificationBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/levelup/levelUpCCVerificationBE/LevelUpCcVerificationBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levelup.levelUpCCVerificationBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelUpCcVerificationBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
